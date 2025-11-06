83fe0f1cb0775cf15ac92553e6aba6700f77b552
eugen888 <bornb228@gmail.coom>
2025-11-06 20:13:34 +0200

    https://kompege.ru/variant?kim=25123352

---
+++ Task-6/22071.py
@@ -0,0 +1,32 @@
+from turtle import *
+screensize(3500, 3500)
+m = 20
+tracer(0)
+fd(30 * m)
+lt(60)
+fd(24 * m)
+rt(240)
+fd(54 * m)
+lt(120)
+fd(24 * m)
+lt(60)
+up()
+fd(30 * m)
+rt(90)
+fd(20 * m)
+lt(90)
+down()
+for i in range(17):
+    fd(6 * m)
+    lt(90)
+    fd(80 * m)
+    lt(90)
+up()
+for x in range(30, 37):
+    for y in range(0, 22):
+        goto(x * m, y * m )
+        dot(3, 'blue')
+update()
+done()
+
+# 49
